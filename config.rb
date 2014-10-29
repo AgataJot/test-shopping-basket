ce0bb04a2b4afc9d39c1c628fecf119d44c0985c
Agata Jewko <agata@jewko.co.uk>
2014-10-29 09:22:30 +0000

    Change to relative paths

--- config.rb
+++ config.rb
@@ -2,11 +2,12 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
+http_path = "./"
 css_dir = "stylesheets"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
+relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
